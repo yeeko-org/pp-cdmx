be7636369d22a8d251bd5247a526d034057efacf
vash512 <phyrox.vash512@gmail.com>
2020-08-31 16:31:22 -0500

    configuracion para el admin de public_account.PPImage

--- pp-cdmx/public_account/admin.py
+++ pp-cdmx/public_account/admin.py
@@ -4,15 +4,37 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from .models import PublicAccount, PPImage
+from project.models import FinalProject
+
 
 class PPImageInline(admin.StackedInline):
-      model = PPImage
-      extra = 0
+    model = PPImage
+    extra = 0
 
 
 class PublicAccountAdmin(admin.ModelAdmin):
     model = PublicAccount
     list_display = [
         "townhall", "period_pp"]
-    inlines=[PPImageInline]
+    inlines = [PPImageInline]
 admin.site.register(PublicAccount, PublicAccountAdmin)
+
+
+class FinalProjectInline(admin.StackedInline):
+    model = FinalProject
+    extra = 0
+    raw_id_fields = ["suburb", "period_pp", "project", "user_validation"]
+
+
+class PPImageAdmin(admin.ModelAdmin):
+    model = PPImage
+    list_display = ["public_account", "path", "status"]
+    list_filter = [
+        "public_account__townhall",
+        "public_account__period_pp__year"]
+    search_fields = [
+        "path", "public_account__townhall__name",
+        "finalproject__suburb__name"]
+    inlines = [FinalProjectInline]
+    raw_id_fields = ["public_account"]
+admin.site.register(PPImage, PPImageAdmin)
